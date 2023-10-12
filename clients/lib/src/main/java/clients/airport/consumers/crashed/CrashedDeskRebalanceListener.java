f8b15341c7ed0709331335d76fc4c76362c009f2
Katie Maison <kmm565@york.ac.uk>
2023-10-13 00:01:04 +0100

    Exercise 1 reformat

--- clients/lib/src/main/java/clients/airport/consumers/crashed/CrashedDeskRebalanceListener.java
+++ clients/lib/src/main/java/clients/airport/consumers/crashed/CrashedDeskRebalanceListener.java
@@ -12,35 +12,35 @@ import org.apache.kafka.common.TopicPartition;
 
 import com.google.common.collect.Table;
 
-public class CrashedDeskRebalanceListener implements ConsumerRebalanceListener{
+public class CrashedDeskRebalanceListener implements ConsumerRebalanceListener {
 
-	 private CrashedDeskConsumer consumer;
+	private CrashedDeskConsumer consumer;
 
-	 public CrashedDeskRebalanceListener(CrashedDeskConsumer consumer) {
-	        this.consumer = consumer;
-	    }
-	
+	public CrashedDeskRebalanceListener(CrashedDeskConsumer consumer) {
+		this.consumer = consumer;
+	}
+
 	@Override
 	public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-		// TODO the consumer should get ready to process messages coming from these partitions
+		// TODO the consumer should get ready to process messages coming from these
+		// partitions
 
-        for (TopicPartition tp : partitions) {
-        	 for (Table.Cell<Integer, Integer, Instant> cell : consumer.lastHeartbeat.cellSet()) {
-                 if (cell.getRowKey() == tp.partition()) {
-                	 consumer.lastHeartbeat.remove(cell.getRowKey(), cell.getColumnKey());
-                 }
-             }    
-        	}
+		for (TopicPartition tp : partitions) {
+			for (Table.Cell<Integer, Integer, Instant> cell : consumer.lastHeartbeat.cellSet()) {
+				if (cell.getRowKey() == tp.partition()) {
+					consumer.lastHeartbeat.remove(cell.getRowKey(), cell.getColumnKey());
+				}
+			}
 		}
-		
+	}
 
 	@Override
 	public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
 		// TODO the consumer should discard any internal state related to these
-		// partitions, and commit anything that the new consumer handling this partition would require Map<Integer, Map<Integer, Instant>> Partition ID -> desk ID -> last status
-		
-	}
-	
-	
-}
+		// partitions, and commit anything that the new consumer handling this partition
+		// would require Map<Integer, Map<Integer, Instant>> Partition ID -> desk ID ->
+		// last status
 
+	}
+
+}
